8d5a71a4fcf78244c2e5c5a35fda29286b3ea7c8
Janoooh <alejandro.orellana.m@mail.pucv.cl>
2025-08-25 21:11:21 -0400

    Inicio de proyecto

---
+++ src/main/java/com/mycompany/siacentrosdesangre/SIACentrosDeSangre.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.siacentrosdesangre;
+
+/**
+ *
+ * @author Usuario
+ */
+public class SIACentrosDeSangre {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
