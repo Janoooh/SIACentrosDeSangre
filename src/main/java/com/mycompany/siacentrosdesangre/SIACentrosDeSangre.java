5c33b5e450c4c3133d3cbf21c639b96f308a7372
Ajorge67 <jorge.ramirez.l@mail.pucv.cl>
2025-08-26 23:03:51 -0400

    prueba

--- src/main/java/com/mycompany/siacentrosdesangre/SIACentrosDeSangre.java
+++ src/main/java/com/mycompany/siacentrosdesangre/SIACentrosDeSangre.java
@@ -1,16 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.siacentrosdesangre;
 
-/**
- *
- * @author Usuario
- */
 public class SIACentrosDeSangre {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.printf("Este es el main");
     }
 }
